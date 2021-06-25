2fb22f7ff895a2bc61e4a7e679027f8e84ae1fa9
Agustin Di Benedetto <agustindibenedetto98@gmail.com>
2021-06-25 03:45:07 -0300

    form widths adjusted

--- scripts/modules/HandleForm.js
+++ scripts/modules/HandleForm.js
@@ -2,18 +2,18 @@ function HandleForm () {
     const $form = document.querySelector(".form");
     $form.addEventListener('send', handleSend);
     async function handleSend(e){
-      e.stopImmediatePropagation();
-      const form = new FormData(this);
-      const response = await fetch(this.action, {
-        method: this.method,
-        body: form,
-        headers: {
-          'Accept': 'application/json'
-        }    
-      });
-      if(response.ok){
-        alert('thx')
-      }
+        const form = new FormData(this);
+        const response = await fetch(this.action, {
+            method: this.method,
+            body: form,
+            headers: {
+                'Accept': 'application/json'
+            }    
+        });
+        e.stopImmediatePropagation();
+        if(response.ok){
+            alert('thx')
+        }
     }
 }
 
